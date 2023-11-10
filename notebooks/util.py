12f898231884ff5257b7cd579876c4404e699451
Yago Lizarribar <yago.lizarribar@gmail.com>
2023-11-10 13:55:53 +0100

    add crlb function

--- notebooks/util.py
+++ notebooks/util.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from pytdoa.geodesy.geodesy import SPEED_OF_LIGHT
+
 
 def make_grid(frequency: int, plot_inf: bool = False, gnbi: int = 1, gnbj: int = 2) -> Tuple[plt.Figure, plt.Axes]:
     fig, ax = plt.subplots(2, 3, figsize=(12, 8))
@@ -77,5 +79,41 @@ def dop(positions: np.ndarray, receivers: np.ndarray) -> np.ndarray:
     Q = np.linalg.inv(np.matmul(np.transpose(H,axes=[2,1,0]),np.transpose(H,axes=[2,0,1])))
     Q = np.transpose(Q,axes=[1,2,0])
     return np.hstack((Q[0,0,:].reshape(-1,1), Q[1,1,:].reshape(-1,1)))
+
+
+def crlb(positions: np.ndarray, receivers: np.ndarray, c: float = SPEED_OF_LIGHT, snr: float = 10, bw: float = 100e6) -> np.ndarray:
+    m = positions.shape[0]
+    n = positions.shape[1]
+    r = receivers.shape[0]
+    assert n == receivers.shape[1], "Incompatible number of dimensions for positions and receivers"
+
+    # We generate the variance for all sensors
+    snr_w = 10 ** (snr/20)
+    sigma = c**2 / (snr_w * bw**2)
+
+    # Covariance matrix
+    R = sigma*np.ones((r-1,r-1))
+    di = np.diag_indices(r-1)
+    R[di] += sigma
+    Ri = np.linalg.inv(R)
+
+    # We generate a 1xnxm hypermatrix and extend it with receivers
+    d_vec = np.transpose(positions.reshape(m,n,1),axes=[2,1,0]) - receivers.reshape(r,n,1)
+    d = np.linalg.norm(d_vec, axis=1)
+    d_vec /= (d.reshape(r,1,m)+1e-3)
+
+    mu = (d_vec[0,:,:] - d_vec[1:,:,:])
+    Ri3 = np.repeat(Ri.reshape((1,r-1,r-1)),m,axis=0)
+    
+    J = np.matmul(np.matmul(mu.transpose([2,1,0]),Ri3),mu.transpose([2,0,1]))
+
+    C = np.linalg.inv(J)
+    Q = np.transpose(C,axes=[1,2,0])
+    return np.hstack((Q[0,0,:].reshape(-1,1), Q[1,1,:].reshape(-1,1)))
+
+
+
+
+
 
 
